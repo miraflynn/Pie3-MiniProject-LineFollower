ea5f4cbf7273d7d2f1c6c8f791ef3a3bbef37da1
nvanmoon <nvanmoon@olin.edu>
2021-10-16 16:37:40 -0400

    Update pie3_motortest.ino

--- pie3_motortest/pie3_motortest.ino
+++ pie3_motortest/pie3_motortest.ino
@@ -6,19 +6,16 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(2);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(1);
 
-//Inititalize Inputs
+//Inititalize Input Pins:
 int leftSensor = A0;
 int rightSensor = A1;
 
 //Set Initial Constants: 
-int  fSpeed = 30;
-int calibrationdifference = -32;
-float turnFactor = 0.01; //Higher means more sensitive/turns more
+int  fSpeed = 30; //forward speed motors are run at when driving straight
+int calibrationdifference = -32; //Manually set calibration factor that accounts for the resting difference between the incoming value of the two sensors
+float turnFactor = 0.01; //Contant to determine feedback sensitivity of turning based on sensor input. Higher means more sensitive/turns more
 
-String inputString = "";         // a String to hold incoming data
-bool stringComplete = false;  // whether the string is complete
-
-
+//Function to drive the motors
 void drive(int lMotor, int rMotor){
   leftMotor->setSpeed(lMotor);
   rightMotor->setSpeed(rMotor);
@@ -27,15 +24,14 @@ void drive(int lMotor, int rMotor){
 }
 
 
-
 void setup() {
-  // put your setup code here, to run once:
   AFMS.begin();
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  inputString.reserve(200); // reserve 200 bytes = 200 characters for the input string
+//  while (!Serial) {
+//    ; // wait for serial port to connect. Needed for native USB port only
+//  }
+// inputString.reserve(200); // reserve 200 bytes = 200 characters for the input string
+
 }
 
 
@@ -43,7 +39,8 @@ void loop() {
 //Serial Port Controls:
   uint8_t incoming;
   incoming = Serial.read();
-  
+
+// Change the Speed
   if (incoming == '+'){
     fSpeed = fSpeed + 10;
     Serial.print("New Speed = ");
@@ -56,6 +53,7 @@ void loop() {
     Serial.println(fSpeed);
    }
 
+// Change the Turn Factor
  if (incoming == 'u'){
     turnFactor = turnFactor + 0.01;
     Serial.print("New P = ");
@@ -68,23 +66,17 @@ if (incoming == 'd'){
     Serial.println(turnFactor);
   }
 
-  
-  // put your main code here, to run repeatedly:
+//Read Both Sensor Values and Find Differnce
   int ls = analogRead(leftSensor);
   int rs = analogRead(rightSensor);
-  int diff = (ls-rs-calibrationdifference);
+  int diff = (ls-rs-calibrationdifference); //calibrated diffence will be how much ls is bigger than rs when both seeing the same terrain
+//Calculate turn speed based on total speed and differnece between sensor values and sensitivity to change
   int turnSpeed = diff*fSpeed*turnFactor;
 
-  drive(fSpeed - turnSpeed, fSpeed+turnSpeed);
-  //Serial.print("driving");
+//Drive based on calculated Turn Speed:
+  drive(fSpeed-turnSpeed, fSpeed+turnSpeed);
+  
+ //Serial.print("driving");
  // Serial.print(forwardSpeed-turnSpeed);
-  //Serial.println(forwardSpeed+turnSpeed);
-
-  if (stringComplete) {
-    Serial.print(inputString);
-    // clear the string:
-    inputString = "";
-    stringComplete = false;
-    Serial.println("Hello computer!");
-  }
+ //Serial.println(forwardSpeed+turnSpeed);
 }
