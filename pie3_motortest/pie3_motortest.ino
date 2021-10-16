ce8b71cb115c69c69038af5aa394e7915aaa21ea
nvanmoon <nvanmoon@olin.edu>
2021-10-16 15:38:43 -0400

    Serial Communications

--- pie3_motortest/pie3_motortest.ino
+++ pie3_motortest/pie3_motortest.ino
@@ -6,13 +6,13 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(2);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(1);
 
+//Inititalize Inputs
 int leftSensor = A0;
 int rightSensor = A1;
 
-//int turnSpeed = 50;
-int forwardSpeed = 30;
+//Set Initial Constants: 
+int  fSpeed = 30;
 int calibrationdifference = -32;
-
 float turnFactor = 0.01; //Higher means more sensitive/turns more
 
 String inputString = "";         // a String to hold incoming data
@@ -26,24 +26,7 @@ void drive(int lMotor, int rMotor){
   rightMotor->run(FORWARD);
 }
 
-/*
-  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
-  routine is run between each time loop() runs, so using delay inside loop can
-  delay response. Multiple bytes of data may be available.
-*/
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
-    if (inChar == '\n') {
-      stringComplete = true;
-    }
-  }
-}
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -55,17 +38,47 @@ void setup() {
   inputString.reserve(200); // reserve 200 bytes = 200 characters for the input string
 }
 
+
 void loop() {
+//Serial Port Controls:
+  uint8_t incoming;
+  incoming = Serial.read();
+  
+  if (incoming == '+'){
+    fSpeed = fSpeed + 10;
+    Serial.print("New Speed = ");
+    Serial.println(fSpeed);
+  }
+
+   if (incoming == '-'){
+    fSpeed = fSpeed - 10;
+    Serial.print("New Speed = ");
+    Serial.println(fSpeed);
+   }
+
+ if (incoming == 'u'){
+    turnFactor = turnFactor + 0.01;
+    Serial.print("New P = ");
+    Serial.println(turnFactor);
+  }
+
+if (incoming == 'd'){
+    turnFactor = turnFactor - 0.01;
+    Serial.print("New P = ");
+    Serial.println(turnFactor);
+  }
+
+  
   // put your main code here, to run repeatedly:
   int ls = analogRead(leftSensor);
   int rs = analogRead(rightSensor);
   int diff = (ls-rs-calibrationdifference);
-  int turnSpeed = diff*forwardSpeed*turnFactor;
+  int turnSpeed = diff*fSpeed*turnFactor;
 
-  drive(forwardSpeed-turnSpeed, forwardSpeed+turnSpeed);
-  Serial.print("driving");
-  Serial.println(forwardSpeed-turnSpeed);
-  Serial.println(forwardSpeed+turnSpeed);
+  drive(fSpeed - turnSpeed, fSpeed+turnSpeed);
+  //Serial.print("driving");
+ // Serial.print(forwardSpeed-turnSpeed);
+  //Serial.println(forwardSpeed+turnSpeed);
 
   if (stringComplete) {
     Serial.print(inputString);
