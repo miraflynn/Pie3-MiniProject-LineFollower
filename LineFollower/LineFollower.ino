486cb821615a495ea4e217d366208e02534cc91c
Mira Flynn <miraflynn0@gmail.com>
2021-10-20 19:53:28 -0400

    Working (tm) line following

--- LineFollower/LineFollower.ino
+++ LineFollower/LineFollower.ino
@@ -1,7 +1,7 @@
 
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
+//#include "utility/Adafruit_MS_PWMServoDriver.h"
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(2);
@@ -15,16 +15,18 @@ int rightSensor = A1;
 int  fSpeed = 35; //forward speed motors are run at when driving straight
 int calibrationdifference = 3; //Manually set calibration factor that accounts for the resting difference between the incoming value of the two sensors
 //float turnFactor = 0.0018;//Contant to determine feedback sensitivity of turning based on sensor input. Higher means more sensitive/turns more
-float turnFactor = 0.005;
+float turnFactor = 0.006;
+int maxSpeedLimit = fSpeed; // Maximum speed a motor is allowed to go
+int minSpeedLimit = -(fSpeed - 10); // Minimum speed a motor is allowed to go
 
 //Function to drive the motors
 void drive(int lMotor, int rMotor) {
-  leftMotor->setSpeed(min(abs(lMotor),fSpeed));
-  rightMotor->setSpeed(min(abs(rMotor),fSpeed));
-  if(lMotor<0){
+  leftMotor->setSpeed(abs(lMotor)); // Set motor speed to absolute value of the input
+  rightMotor->setSpeed(abs(rMotor));
+  if(lMotor<0){ // If negative, run backward
     leftMotor->run(BACKWARD);
   } else{
-    leftMotor->run(FORWARD);
+    leftMotor->run(FORWARD); // If nonnegative, run forward
   }
   if(rMotor<0){
     rightMotor->run(BACKWARD);
@@ -44,21 +46,21 @@ void setup() {
 
 
 
-  int ls = analogRead(leftSensor);
-  int rs = analogRead(rightSensor);
-  int diff = (ls - rs - calibrationdifference);
-  Serial.print("left:");
-  Serial.println(ls) ;
-  Serial.print("right:");
-  Serial.println(rs);
-  Serial.print("raw diff:");
-  Serial.println(ls - rs);
-  Serial.print("claibrated dff (should be 0):");
-  Serial.println(diff);
-  Serial.print("speed:");
-  Serial.println(fSpeed);
-  Serial.print("p:");
-  Serial.println(turnFactor, 4);
+//  int ls = analogRead(leftSensor);
+//  int rs = analogRead(rightSensor);
+//  int diff = (ls - rs - calibrationdifference);
+//  Serial.print("left:");
+//  Serial.println(ls) ;
+//  Serial.print("right:");
+//  Serial.println(rs);
+//  Serial.print("raw diff:");
+//  Serial.println(ls - rs);
+//  Serial.print("claibrated dff (should be 0):");
+//  Serial.println(diff);
+//  Serial.print("speed:");
+//  Serial.println(fSpeed);
+//  Serial.print("p:");
+//  Serial.println(turnFactor, 4);
 
 }
 
@@ -101,20 +103,30 @@ void loop() {
   //Calculate turn speed based on total speed and differnece between sensor values and sensitivity to change
   int turnSpeed = diff * fSpeed * turnFactor;
 
-  //Drive based on calculated Turn Speed:
-  drive((fSpeed - turnSpeed) * 16 / 17, fSpeed + turnSpeed);
+  int leftSpeed = (fSpeed - turnSpeed);
+  int rightSpeed = (fSpeed + turnSpeed);
 
-  //Print Motor Speeds:
-  Serial.print("Left_Motor:");
-  Serial.print((fSpeed - turnSpeed) * 16 / 17); //also multiplied by 16/17 but this is to account for a hardware difference
-  Serial.print(", ");
-  Serial.print("Right_Motor:");
-  Serial.print(fSpeed + turnSpeed);
-  Serial.print(", ");
-  Serial.print("Left_Sensor:");
+  leftSpeed = max(leftSpeed, minSpeedLimit); // Limit the leftSpeed to within the global speed limits
+  leftSpeed = min(leftSpeed, maxSpeedLimit);
+  leftSpeed = leftSpeed * 16/17; // Accounting for hardware difference between these motors
+
+  rightSpeed = max(rightSpeed, minSpeedLimit); // Limit the rightSpeed to within the global speed limits
+  rightSpeed = min(rightSpeed, maxSpeedLimit);
+  
+  //Drive based on calculated Turn Speed:
+  drive(leftSpeed, rightSpeed);
+
+  //Print Motor Speeds in CSV format:
+//  Serial.print("Left_Motor:");
+  Serial.print(leftSpeed);
+  Serial.print(",");
+//  Serial.print("Right_Motor:");
+  Serial.print(rightSpeed);
+  Serial.print(",");
+//  Serial.print("Left_Sensor:");
   Serial.print(ls);
-  Serial.print(", ");
-  Serial.print("Right_Sensor:");
+  Serial.print(",");
+//  Serial.print("Right_Sensor:");
   Serial.println(rs);
 
 
